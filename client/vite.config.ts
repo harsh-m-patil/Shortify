0b55b9593b9d991fdf7def35dfe9d54c1d62df7a
Harshwardhan <135024692+harsh-m-patil@users.noreply.github.com>
2024-10-30 12:23:00 +0530

    chore:run sv create

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vitest/config";
+import { sveltekit } from '@sveltejs/kit/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [sveltekit()],
+
+    test: {
+        include: ['src/**/*.{test,spec}.{js,ts}']
+    }
+});
